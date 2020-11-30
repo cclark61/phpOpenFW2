587d25335b2702b8a64c7bbe19e601d09252424c
Christian Clark <cclark61@gmail.com>
2020-11-30 17:08:08 -0500

    Small update.

--- src/plugins/Format/Image.php
+++ src/plugins/Format/Image.php
@@ -6,7 +6,7 @@
  *
  * @package        phpOpenFW
  * @author         Christian J. Clark
- * @copyright    Copyright (c) Christian J. Clark
+ * @copyright      Copyright (c) Christian J. Clark
  * @license        https://mit-license.org
  **/
 //**************************************************************************************
